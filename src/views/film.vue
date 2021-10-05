a007d099aad03b8468e769800b15274495af4b33
Brian Ford <brianfordcode@gmail.com>
2021-10-05 14:28:20 -0700

    add film details

--- src/views/film.vue
+++ src/views/film.vue
@@ -17,8 +17,9 @@
 
         <div class="summary">
           <div class="summary-text">
-            <span>{{ film.title }}</span>
+            <span class="title">{{ film.title }}</span>
             <span>{{ film.description }}</span>
+            <span>Roles: {{ film.roles }}</span>
           </div>
           <div class="awards"
               v-if="film.award1"
@@ -26,6 +27,8 @@
             <img style="width: 150px" :src="`${film.award1}`" alt="award">
             <img style="width: 150px" :src="`${film.award2}`" alt="award">
           </div>
+          <span v-if="film.websiteTitle">Website: <a :href="`${film.websiteURL}`" target="_blank">{{film.websiteTitle}}</a></span>
+          
         </div>
 
       </div>
@@ -48,34 +51,52 @@ export default {
         {
           url: "https://player.vimeo.com/video/77258876?h=14ac82ce37",
           title: "Home Front",
-          description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          description: "10 Minute short. An American mother and son cope with the absence of the father who is fighting overseas during WWII.",
+          roles: "Screenwriter, Director, Editor",
           award1: require("../assets/awards/gi-laurel.png"),
           award2: require("../assets/awards/high-desert-laurel.png"),
+          websiteTitle: "IMDb Page",
+          websiteURL: "https://www.imdb.com/title/tt2424882/?ref_=fn_tt_tt_19",
         },
         {
           url: "https://player.vimeo.com/video/77258875?h=d067f71b65",
           title: "Brant Cotton - See If I Care",
-          description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          description: "Music Video for country singer Brant Cotton. Shot on location in the Santa Ynez Valley, CA",
+          roles: "Director, Editor",
+          websiteTitle: "Brant Cotton",
+          websiteURL: "https://www.facebook.com/Brantcotton/",
         },
         {
           url: "https://player.vimeo.com/video/573268188?h=ada4683bc1",
           title: "Xinspire Promo Video",
           description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          roles: "Director, Editor, Camera",
+          websiteTitle: "Xinspire",
+          websiteURL: "https://www.xinspire.com/",
         },
         {
           url: "https://player.vimeo.com/video/573274760?h=8151271e35",
           title: "Brunch at Sandbar",
           description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          roles: "Director, Producer, Editor,",
+          websiteTitle: "Sandbar Santa Barbara",
+          websiteURL: "https://sandbarsb.com/",
         },
         {
           url: "https://player.vimeo.com/video/584613989?h=77c56203df",
           title: "House Tour for Real Estate",
           description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          roles: "Director, Editor, Camera",
+          websiteTitle: "",
+          websiteURL: "",
         },
         {
           url: "https://player.vimeo.com/video/572907199?h=83e4423f22",
           title: "EPA Dream Center Promo Video",
           description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. A voluptatum tempora quam ratione maiores itaque harum sed culpa provident expedita.",
+          roles: "Editor",
+          websiteTitle: "EPA Dream Center",
+          websiteURL: "https://www.epadreamcenter.org/",
         },
       ]
     }
@@ -103,7 +124,7 @@ export default {
 .film-wrapper {
   display: flex;
   align-items: center;
-  margin-bottom: 30px;
+  margin-bottom: 50px;
   /* border: 1px solid green; */
   position: relative;
 }
@@ -133,10 +154,24 @@ span {
   padding-bottom: 20px;
 }
 
+.title {
+  font-size: 30px;
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+
+a:hover{
+  color: grey;
+}
+
 .awards {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding-bottom:20px;
 }
 
 </style>
