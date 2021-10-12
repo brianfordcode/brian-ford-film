f0a6f0416b3902cbdc40fa267d97bd2cf7d061d7
Brian Ford <brianfordcode@gmail.com>
2021-10-12 15:08:16 -0700

    add favicon

--- src/router/index.js
+++ src/router/index.js
@@ -5,34 +5,56 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      title: "Brian Ford Film | Home"
+    }
   },
   {
     path: '/about',
     name: 'About',
-    component: () => import('../views/About.vue')
+    component: () => import('../views/About.vue'),
+    meta: {
+      title: "Brian Ford Film | About"
+    }
   },
   {
     path: '/photography',
     name: 'Photography',
-    component: () => import('../views/photography.vue')
+    component: () => import('../views/photography.vue'),
+    meta: {
+      title: "Brian Ford Film | Photography"
+    }
   },
   {
     path: '/film',
     name: 'Film',
-    component: () => import('../views/film.vue')
+    component: () => import('../views/film.vue'),
+    meta: {
+      title: "Brian Ford Film | Film"
+    }
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import('../views/contact.vue')
+    component: () => import('../views/contact.vue'),
+    meta: {
+      title: "Brian Ford Film | Contact"
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  mode: "history",
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return { top: 0 }
+  }
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title}`
+  next();
 })
 
 export default router
